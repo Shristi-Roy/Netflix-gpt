2a6eccae385bf795e4dc5a34c019dabc6048a94c
Shristi-Roy <shristi456roy@gmail.com>
2023-12-20 15:13:33 +0530

    signIn/Up form && Login/Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Login from './Login'
+
+const Header = () => {
+  return (
+    <div>
+      <Login/>
+
+      <div className='absolute px-8 py-2 bg-gradiant-to-b from-black z-30' >
+        <img className='w-44'
+        src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
